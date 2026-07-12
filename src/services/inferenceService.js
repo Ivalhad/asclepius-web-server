50a01af087ad1cf0fff6439e13cf4f4254b4d559
ivalhad <ivan14hadrian@gmail.com>
2026-07-12 19:34:38 +0700

    feat: implement inference service using tfjs-node for image classification and update dependencies

--- src/services/inferenceService.js
+++ src/services/inferenceService.js
@@ -4,37 +4,8 @@ const InputError = require('../exceptions/InputError');
 async function predictClassification(model, image) {
     try {
 
-        let imageBuffer;
-        if (Buffer.isBuffer(image)) {
-            imageBuffer = image;
-        } else if (image._data) {
-
-            imageBuffer = image._data;
-        } else if (typeof image.pipe === 'function') {
-
-            const chunks = [];
-            for await (const chunk of image) {
-                chunks.push(chunk);
-            }
-            imageBuffer = Buffer.concat(chunks);
-        } else {
-            throw new InputError('Terjadi kesalahan dalam melakukan prediksi');
-        }
-
-        const jpeg = require('jpeg-js');
-        const { width, height, data } = jpeg.decode(imageBuffer, { useTArray: true });
-
-        const numChannels = 3;
-        const numPixels = width * height;
-        const rgbValues = new Int32Array(numPixels * numChannels);
-
-        for (let i = 0; i < numPixels; i++) {
-            for (let c = 0; c < numChannels; c++) {
-                rgbValues[i * numChannels + c] = data[i * 4 + c];
-            }
-        }
-
-        const tensor = tf.tensor3d(rgbValues, [height, width, numChannels], 'int32')
+        const tensor = tf.node
+            .decodeJpeg(image)
             .resizeNearestNeighbor([224, 224])
             .expandDims()
             .toFloat();
